9940245b8cef1c486dccd02dba2148ebe45afcf0
Joshua La <j0shua001@outlook.com>
2023-12-05 06:24:39 -0500

    main menu added

---
+++ Assets/Underwater Diving/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target; // The target that the camera follows
+    public float smoothTime = 0.3f; // Time for the camera to reach the target
+    public float cameraZ;
+
+    private Vector3 velocity = Vector3.zero; // Velocity reference, used by SmoothDamp
+
+    void Update()
+    {
+        if (target != null)
+        {
+            Vector3 targetPosition = target.position;
+
+            // Modify the target position as needed (e.g., add offset, limit to certain axes, etc.)
+            targetPosition.z = cameraZ;
+
+            // Smoothly move the camera towards the target position
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+    }
+}
